5fefe484ce85dd6f81317787872b29a89ce4c765
daniellef <daniellefarias813@gmail.com>
2015-11-02 10:06:33 -0800

    i created a few variables

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -1,7 +1,18 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+var snake;
 
+var context;
+var screenWidth;
+var screenHeight;
 
+function gameInitialize() {
+    
+}
+
+function gameLoop() {
+    
+}
+
+ function gameDraw () {
+     
+ }
+ 
