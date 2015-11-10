c89412bff19d920bad2176cf54175c1be2566445
daniellef <daniellefarias813@gmail.com>
2015-11-10 08:42:13 -0800

    i drew the snake

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -10,6 +10,7 @@ var screenHeight;
 
 gameInitialize();
 snakeInitialize();
+foodInitialize();
 setInterval(gameLoop, 1000/30);
 
 function gameInitialize() {
@@ -27,6 +28,7 @@ function gameLoop() {
     gameDraw();
     snakeUpdate();
     snakeDraw();
+    foodDraw();
 }
 
 function gameDraw () {
@@ -72,5 +74,6 @@ function foodInitialize() {
 }
 
 function foodDraw() {
-    
+    context.fillStyle = "white";
+    context.fillRect(food.x, food.y, snakeSize,  snakeSize);
 }
