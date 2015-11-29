31ff839309e49ff8a99e588967fd071edf8c978b
daniellef <daniellefarias813@gmail.com>
2015-11-29 14:25:44 -0800

    im centering the game over menu using js

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -45,6 +45,7 @@ function gameInitialize() {
    document.addEventListener("keydown", keyboardHandler);
    
    gameOverMenu = document.getElementById("gameOver");
+   centerMenuPosition(gameOverMenu);
    
    setState("PLAY");
 }
@@ -198,6 +199,10 @@ function setState(state) {
     showMenu(state);
 }
 
+/*-----------------------------------------------------------------------------------------
+ * Menu Functions
+ *----------------------------------------------------------------------------------------- 
+ */
 function displayMenu(menu) {
     menu.style.visibility = "visible";
 }
@@ -206,4 +211,8 @@ function showMenu(state) {
     if(state == "GAME OVER") {
         displayMenu(gameOverMenu);
     }
+}
+
+function centerMenuPosition(menu) {
+    menu.style.top = (screenHeight /  2) + "px";
 }
